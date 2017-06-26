79ab85e6850e7820e9a5bee2efaf3c61669c2c3e
vadim <vadim@jam3.com>
2017-06-26 15:13:31 -0400

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+Cross-browser fullscreen handler utility.
+Works in IE10+ and mobile browsers (Android - Chrome , iOS - Chrome + Safari)
+
+## Syntax
+fullscreenHandler(element [, onEnterCallback, onExitCallback]);
+
+## API
+```enter``` - enter fullscreen
+```exit``` - exit fullscreen
+```destroy``` remove all even listeners
+
+
+## Example
+```javascript
+
+import fullscreenHandler from 'fullscreen-handler';
+
+const video = document.querySelector('video');
+const fs = fullscreenHandler(video);
+
+video.addEventListener('play', fs.enter);
+video.addEventListener('pause', fs.exit);
+
+...
+
+// make sure to destroy when it's time
+fs.destroy();
+
+```
+
+## Install
+```sh
+npm install fullscreen-handler --save
+``` 
+
+## License
+MIT, see [LICENSE.md](http://github.com/Jam3/fullscreen-handler/blob/master/LICENSE) for details.
---
+++ index.js
@@ -0,0 +1,67 @@
+module.exports = function(el, onEnter, onExit,) {
+  init();
+
+  function init() {
+    document.addEventListener('fullscreenchange', onFullScreenChange);
+    document.addEventListener('mozfullscreenchange', onFullScreenChange);
+    document.addEventListener('webkitfullscreenchange', onFullScreenChange);
+    document.addEventListener('MSFullscreenChange', onFullScreenChange);
+    el.addEventListener('webkitendfullscreen', onExit);
+  }
+
+  function destroy() {
+    document.removeEventListener('fullscreenchange', onFullScreenChange);
+    document.removeEventListener('mozfullscreenchange', onFullScreenChange);
+    document.removeEventListener('webkitfullscreenchange', onFullScreenChange);
+    document.removeEventListener('MSFullscreenChange', onFullScreenChange);
+    el.removeEventListener('webkitendfullscreen', onExit);
+  }
+
+  function onFullScreenChange() {
+    if (isFullScreen()) {
+      onEnter && onEnter();
+    } else {
+      onExit && onExit();
+    }
+  }
+
+  function isFullScreen() {
+    return (
+      document.fullscreen ||
+      document.fullScreen ||
+      document.webkitIsFullScreen ||
+      document.mozFullScreen ||
+      (document.msFullscreenElement !== undefined && document.msFullscreenElement !== null)
+    );
+  }
+
+  return {
+    enter: function() {
+      var requestFullScreen = (
+        el.requestFullScreen ||
+        el.mozRequestFullScreen ||
+        el.webkitRequestFullScreen ||
+        el.webkitRequestFullscreen ||
+        el.webkitEnterFullScreen ||
+        el.msRequestFullscreen ||
+        el.requestFullscreen
+      );
+      requestFullScreen && requestFullScreen.call(el);
+    },
+    exit: function() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (el.webkitExitFullScreen) {
+        el.webkitExitFullScreen();
+      }
+    },
+    isFullScreen: isFullScreen,
+    destroy: destroy
+  };
+};
